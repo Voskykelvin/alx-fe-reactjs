0e40952b20463456e12744881a4a2f0485907975
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-08-30 14:36:11 +0300

    Initial commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,8 +4,6 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
-export default App;
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -45,5 +43,4 @@ function App() {
     </>
   )
 }
-
-export default App
+export default App;
