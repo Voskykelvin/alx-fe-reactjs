d1b8ce8f8193d1179be391b3fa91d5afe93f92f1
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-09-14 19:42:18 +0300

    Initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+// MainContent.jsx
+import React from "react";
+
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'navy', color: 'black', textAlign: 'Left' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
