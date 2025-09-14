7648c8acccb8974fdfa739b24d15a8f4cc378dc9
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-09-14 22:12:38 +0300

    Initial commit

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: "40px",
+        padding: "10px",
+        background: "#f0f0f0",
+        textAlign: "center",
+      }}
+    >
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "15px",
+        background: "#333",
+        display: "flex",
+        gap: "15px",
+      }}
+    >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
