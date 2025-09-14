2d96da9673b56b55aa077d1d20362ce86cc8372e
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-09-14 19:56:24 +0300

    Initial commit

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function MainContent() {
   return (
-    <main style={{ backgroundColor: 'navy', color: 'black', textAlign: 'Left' }}>
+    <main style={{ backgroundColor: 'navy', color: 'Purple', textAlign: 'Left' }}>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
