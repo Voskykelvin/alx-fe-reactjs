dae603716cea42926e6d7d349452a8ed092e8a54
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-09-21 20:40:05 +0300

    Initial commit

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RepoList from "./components/RepoList";
+import search from ">?components/search";
 
 function App() {
   return (
     <Router>
       <div>
+      <h1>Github user</h1>
         <nav>
           <Link to="/">Home</Link> | <Link to="/repos">Repositories</Link>
         </nav>
@@ -14,6 +16,7 @@ function App() {
           <Route path="/repos" element={<RepoList />} />
         </Routes>
       </div>
+      <search />
     </Router>
   );
 }
