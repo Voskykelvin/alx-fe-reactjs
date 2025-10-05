d4b88c48d1943dc2a69e67ed7f7e6f925f2a2561
Kelvin Ochieng <kelvinvosky2@gmail.com>
2025-10-05 20:43:30 +0300

    Initial commit

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
          Featured Recipes
       </h1>
 
-      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-8 sm:grid-cols-1 m:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
